cc5f5d0266149ade07c3db22f17a3268bb726b8d
Mauro Daprotis <maurodaprotis@gmail.com>
2022-06-11 21:15:59 -0300

    feat: add brewfile

--- setup-homebrew.zsh
+++ setup-homebrew.zsh
@@ -4,12 +4,4 @@ echo "\n🍺 Starting Homebrew Setup \n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install bat
-brew install gnupg
-brew install nvm
-brew install pyenv
-
-brew install --cask iterm2
-brew install --cask visual-studio-code
-brew install --cask brave-browser
-brew install --cask insomnia
+brew bundle --verbose
